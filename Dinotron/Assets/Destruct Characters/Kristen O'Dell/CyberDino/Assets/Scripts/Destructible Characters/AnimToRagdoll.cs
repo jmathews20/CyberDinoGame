f9619cd1e0d45012c19392afd8c840ba74e12f70
Unknown <little_miss_kris07@hotmail.com>
2017-04-14 20:58:17 -0600

    Destructible Characters

---
+++ Dinotron/Assets/Destruct Characters/Kristen O'Dell/CyberDino/Assets/Scripts/Destructible Characters/AnimToRagdoll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AnimToRagdoll : MonoBehaviour
+{
+    //This script makes the dinosaur turn into a ragdoll when it dies
+    public GameObject ragdollDino;
+    public GameObject animDinosaur;
+    public Image healthBar;
+	void Start ()
+    {
+        ragdollDino.SetActive(false);
+	}
+	
+	void Update ()
+    {
+        ragdollDino.transform.position = animDinosaur.transform.position;
+        if (healthBar.fillAmount <= 0)
+        {
+            ragdollDino.SetActive(true);
+            animDinosaur.SetActive(false);
+        }
+    }
+}
+/* In the editor add the health bar image to the dinosaur.
+Add this script to the dinosaur.*/
